32d3df77bb46759b03620063e81ce0f98a795144
Crasneanu Cristian <crasneanu.cristian@gmail.com>
2019-09-10 11:37:26 +0300

    [ Update ] STPhotoCollection (0.0.4)

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "5.0")),
         .package(url: "https://github.com/mikelanza/st-photo-core-ios.git", .upToNextMajor(from: "0.1.2")),
         .package(url: "https://github.com/mikelanza/st-photo-details-ios.git", .upToNextMajor(from: "0.0.8"))
-        .package(url: "https://github.com/mikelanza/st-photo-collection-ios.git", .upToNextMajor(from: "0.0.3"))
+        .package(url: "https://github.com/mikelanza/st-photo-collection-ios.git", .upToNextMajor(from: "0.0.4"))
     ],
     targets: [
         .target(
