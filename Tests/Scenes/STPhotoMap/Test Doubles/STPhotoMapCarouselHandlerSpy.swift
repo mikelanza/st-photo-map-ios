967adc42f48d8943842d0106ab192ad9ae5efeb8
Dimitri Strauneanu <strauneanu.dimitri@gmail.com>
2019-09-13 10:53:24 +0300

    [ Interactor Unit Tests ] - Refactor unit tests for carousel reloading

--- Tests/Scenes/STPhotoMap/Test Doubles/STPhotoMapCarouselHandlerSpy.swift
+++ Tests/Scenes/STPhotoMap/Test Doubles/STPhotoMapCarouselHandlerSpy.swift
@@ -12,6 +12,7 @@ class STPhotoMapCarouselHandlerSpy: STPhotoMapCarouselHandler {
     var addActiveDownloadCalled: Bool = false
     var removeActiveDownloadCalled: Bool = false
     var removeAllActiveDownloadsCalled: Bool = false
+    var resetCarouselCalled: Bool = false
     
     override func addActiveDownload(_ url: String) {
         self.addActiveDownloadCalled = true
@@ -23,5 +24,9 @@ class STPhotoMapCarouselHandlerSpy: STPhotoMapCarouselHandler {
     
     override func removeAllActiveDownloads() {
         self.removeAllActiveDownloadsCalled = true
+    }
+    
+    override func resetCarousel() {
+        self.resetCarouselCalled = true
     }
 }
