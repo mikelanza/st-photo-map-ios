2a66282228e5598ecde196bbec210c4387a66b12
Dimitri Strauneanu <strauneanu.dimitri@gmail.com>
2019-09-13 12:04:59 +0300

    [ Interactor Unit Tests ] - Refactor unit tests for photo annotation selection

---
+++ Tests/Scenes/STPhotoMap/Interactor/STPhotoMapInteractorTests+PhotoAnnotationSelection.swift
@@ -0,0 +1,95 @@
+//
+//  STPhotoMapInteractorTests+PhotoAnnotationSelection.swift
+//  STPhotoMapTests-iOS
+//
+//  Created by Dimitri Strauneanu on 13/09/2019.
+//  Copyright © 2019 mikelanza. All rights reserved.
+//
+
+@testable import STPhotoMap
+import XCTest
+import MapKit
+import STPhotoCore
+
+class STPhotoMapInteractorPhotoAnnotationSelectionTests: XCTestCase {
+    var sut: STPhotoMapInteractor!
+    var presenterSpy: STPhotoMapPresentationLogicSpy!
+    var workerSpy: STPhotoMapWorkerSpy!
+    
+    // MARK: - Test lifecycle
+    
+    override func setUp() {
+        super.setUp()
+        self.setupSTPhotoMapInteractor()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    // MARK: - Test setup
+    
+    func setupSTPhotoMapInteractor() {
+        self.sut = STPhotoMapInteractor()
+        
+        self.presenterSpy = STPhotoMapPresentationLogicSpy()
+        self.sut.presenter = self.presenterSpy
+        
+        self.workerSpy = STPhotoMapWorkerSpy(delegate: self.sut)
+        self.sut.worker = self.workerSpy
+    }
+    
+    // MARK: - Tests
+    
+    func testShouldSelectPhotoAnnotationShouldAskThePresenterToPresentNavigateToPhotoDetailsWhenThePhotoAnnotationIsSelected() {
+        let photoAnnotation = STPhotoMapSeeds().photoAnnotation()
+        photoAnnotation.isSelected = true
+        self.sut.shouldSelectPhotoAnnotation(request: STPhotoMapModels.PhotoAnnotationSelection.Request(photoAnnotation: photoAnnotation, previousPhotoAnnotation: nil))
+        XCTAssertTrue(self.presenterSpy.presentNavigateToPhotoDetailsCalled)
+    }
+    
+    func testShouldSelectPhotoAnnotationShouldAskThePresenterToPresentDeselectPhotoClusterAnnotationWhenEntityLevelIsLocationAndThePhotoAnnotationIsNotSelected() {
+        self.sut.entityLevelHandler.entityLevel = .location
+        self.workerSpy.photo = STPhotoMapSeeds().photo()
+        let photoAnnotation = STPhotoMapSeeds().photoAnnotation()
+        photoAnnotation.isSelected = false
+        self.sut.shouldSelectPhotoAnnotation(request: STPhotoMapModels.PhotoAnnotationSelection.Request(photoAnnotation: photoAnnotation, previousPhotoAnnotation: nil))
+        XCTAssertTrue(self.presenterSpy.presentDeselectPhotoClusterAnnotationCalled)
+    }
+    
+    func testShouldSelectPhotoAnnotationShouldAskThePresenterToPresentDeselectPhotoAnnotationWhenEntityLevelIsLocationAndThePhotoAnnotationIsNotSelected() {
+        self.sut.entityLevelHandler.entityLevel = .location
+        self.workerSpy.photo = STPhotoMapSeeds().photo()
+        let photoAnnotation = STPhotoMapSeeds().photoAnnotation()
+        photoAnnotation.isSelected = false
+        self.sut.shouldSelectPhotoAnnotation(request: STPhotoMapModels.PhotoAnnotationSelection.Request(photoAnnotation: photoAnnotation, previousPhotoAnnotation: nil))
+        XCTAssertTrue(self.presenterSpy.presentDeselectPhotoAnnotationCalled)
+    }
+    
+    func testShouldSelectPhotoAnnotationShouldAskThePresenterToPresentSelectPhotoAnnotationWhenEntityLevelIsLocationAndThePhotoAnnotationIsNotSelected() {
+        self.sut.entityLevelHandler.entityLevel = .location
+        self.workerSpy.photo = STPhotoMapSeeds().photo()
+        let photoAnnotation = STPhotoMapSeeds().photoAnnotation()
+        photoAnnotation.isSelected = false
+        self.sut.shouldSelectPhotoAnnotation(request: STPhotoMapModels.PhotoAnnotationSelection.Request(photoAnnotation: photoAnnotation, previousPhotoAnnotation: nil))
+        XCTAssertTrue(self.presenterSpy.presentSelectPhotoAnnotationCalled)
+    }
+    
+    func testShouldSelectPhotoAnnotationShouldAskThePresenterToPresentLoadingStateWhenEntityLevelIsLocationAndThePhotoAnnotationIsNotSelected() {
+        self.sut.entityLevelHandler.entityLevel = .location
+        self.workerSpy.photo = STPhotoMapSeeds().photo()
+        let photoAnnotation = STPhotoMapSeeds().photoAnnotation()
+        photoAnnotation.isSelected = false
+        self.sut.shouldSelectPhotoAnnotation(request: STPhotoMapModels.PhotoAnnotationSelection.Request(photoAnnotation: photoAnnotation, previousPhotoAnnotation: nil))
+        XCTAssertTrue(self.presenterSpy.presentLoadingStateCalled)
+    }
+    
+    func testShouldSelectPhotoAnnotationShouldAskTheWorkerToGetPhotoDetailsForPhotoAnnotationWhenEntityLevelIsLocationAndThePhotoAnnotationIsNotSelected() {
+        self.sut.entityLevelHandler.entityLevel = .location
+        self.workerSpy.photo = STPhotoMapSeeds().photo()
+        let photoAnnotation = STPhotoMapSeeds().photoAnnotation()
+        photoAnnotation.isSelected = false
+        self.sut.shouldSelectPhotoAnnotation(request: STPhotoMapModels.PhotoAnnotationSelection.Request(photoAnnotation: photoAnnotation, previousPhotoAnnotation: nil))
+        XCTAssertTrue(self.workerSpy.getPhotoDetailsForPhotoAnnotationCalled)
+    }
+}
